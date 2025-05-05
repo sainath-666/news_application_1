bc28358a72517f4bc3fb3534ed0636b963f92bf6
Sainathreddy <sai65265@gmail.com>
2025-05-06 00:21:59 +0530

    used sampleOutput.json to test the app

--- src/components/News.js
+++ src/components/News.js
@@ -1,15 +1,25 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is News component
-        <NewsItem />
+      <div className="container my-3">
+        <h2 className="text-center">News - Top Headlines</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title="Title 1" description="Description 1" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="Title 1" description="Description 1" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="Title 1" description="Description 1" />
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;
