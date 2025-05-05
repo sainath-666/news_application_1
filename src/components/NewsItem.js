de417cfb82d5d1695a1b949f4f4e5964835c4ac9
Sainathreddy <sai65265@gmail.com>
2025-05-06 01:45:35 +0530

    fetch data from newsapi

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,7 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img src={!imageUrl?"https://sadbhawnapaati.com/wp-content/uploads/2021/03/news.jpg":imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
